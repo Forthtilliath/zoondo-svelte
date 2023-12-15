6898ced5cbbff4de5894c8483ee451f977d1e759
Forthtilliath <forth@live.fr>
2023-12-15 19:33:50 +0100

    feat: add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,19 @@
+<section>
+	<h1>Une erreur est survenue</h1>
+	<p>Une erreur est survenue. Dégage de là !</p>
+	<a href="/">Retourner sur le board</a>
+</section>
+
+<style>
+	section {
+		display: flex;
+		flex-direction: column;
+		justify-content: center;
+		align-items: center;
+		flex: 1;
+	}
+
+	h1 {
+		font-size: larger;
+	}
+</style>
