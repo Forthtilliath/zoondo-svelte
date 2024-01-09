7abdcf43188804979e086ddd61a0d193ab34d726
Forthtilliath <forth@live.fr>
2024-01-09 17:51:19 +0100

    feat: move file + add draggable to false

---
+++ src/routes/(components)/game/CardTokenOpponent.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+</script>
+
+<img src="/assets/card-back-board.webp" alt="Unknown card owned by opponent" draggable="false" />
+
+<style lang="scss">
+</style>
--- src/routes/CardTokenOpponent.svelte
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-</script>
-
-<img src="/assets/card-back-board.webp" alt="Unknown card owned by opponent" />
-
-<style lang="scss">
-</style>
