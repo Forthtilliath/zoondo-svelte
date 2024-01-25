7a64e00f5f248db1a388bfdb05c5cb0b309ffa28
Forthtilliath <forth@live.fr>
2024-01-25 01:23:13 +0100

    feat: set gameId by a real gameid

--- src/routes/(components)/Hotlinks.svelte
+++ src/routes/(components)/Hotlinks.svelte
@@ -1,6 +1,6 @@
 <nav>
 	<a href="/" class="Logo">Next Template</a>
-	<a href="/games/1">Current (only) game</a>
+	<a href="/games/97a946aa-4a7d-4cef-8b70-da3d6fc70301">Current (only) game</a>
 	<a href="/">Link #2</a>
 	<a href="/">Link #3</a>
 </nav>
