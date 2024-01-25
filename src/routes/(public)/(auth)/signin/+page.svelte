18172bf075672f3a51a5e3b17a0462209caf4b0a
Forthtilliath <forth@live.fr>
2024-01-25 03:19:04 +0100

    feat: add buttons to log faster

--- src/routes/(public)/(auth)/signin/+page.svelte
+++ src/routes/(public)/(auth)/signin/+page.svelte
@@ -1,15 +1,31 @@
 <script lang="ts">
-	import { enhance } from "$app/forms";
+	import { enhance } from '$app/forms';
 
-	export let form;
+	// export let form;
+
+	let buttonElement: HTMLInputElement;
+	let username: string;
+	let password = 'tototo';
+
+	function log(name: string) {
+		username = name;
+		setTimeout(() => buttonElement.click(), 100);
+	}
 </script>
 
 <h1>Sign in</h1>
+
+<div>
+	<button on:click={() => log('ValhallaCode')}>Log VCode</button>
+	<button on:click={() => log('Guillaume')}>Log Guillaume</button>
+	<button on:click={() => log('Forth')}>Log Forth</button>
+</div>
+
 <form method="post" use:enhance>
 	<label for="username">Username</label>
-	<input name="username" id="username" /><br />
+	<input name="username" id="username" bind:value={username} /><br />
 	<label for="password">Password</label>
-	<input type="password" name="password" id="password" /><br />
-	<input type="submit" />
+	<input type="password" name="password" id="password" bind:value={password} /><br />
+	<input type="submit" bind:this={buttonElement} />
 </form>
 <a href="/signup">Create an account</a>
