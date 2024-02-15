85ef294c1357945914135523e77173ed7132926b
Forthtilliath <forth@live.fr>
2024-02-15 18:42:14 +0100

    feat: remove await part since data.board is not anymore a promise

--- src/routes/(protected)/games/[id]/+page.svelte
+++ src/routes/(protected)/games/[id]/+page.svelte
@@ -1,12 +1,13 @@
 <script lang="ts">
-	import {page} from '$app/stores'
+	import { page } from '$app/stores';
 	import Board from './(components)/Board.svelte';
 	import CardSample from './(components)/CardSample.svelte';
 	import Chat from '$lib/components/Chat.svelte';
 
 	export let data;
 	$: ({
-		user: { userId }
+		user: { userId },
+		board
 	} = data);
 </script>
 
@@ -15,23 +16,23 @@
 </svelte:head>
 
 <main>
-	{#await data.board}
+	<!-- {#await data.board}
 		<p>Loading...</p>
-	{:then board} 
-		{#if board}
-			<div class="Board">
-				<Board {board} {userId} room="game#{$page.params.id}"/>
-			</div>
-			<div class="CardSample">
-				<CardSample />
-			</div>
-		{/if}
-		<div class="Chat">
-			<Chat room="game#{$page.params.id}" {userId} />
+	{:then board}  -->
+	{#if board}
+		<div class="Board">
+			<Board {board} {userId} room="game#{$page.params.id}" />
 		</div>
-	{:catch err}
+		<div class="CardSample">
+			<CardSample />
+		</div>
+	{/if}
+	<div class="Chat">
+		<Chat room="game#{$page.params.id}" {userId} />
+	</div>
+	<!-- {:catch err}
 		<p>Error: {err.message}</p>
-	{/await}
+	{/await} -->
 </main>
 
 <style lang="scss">
