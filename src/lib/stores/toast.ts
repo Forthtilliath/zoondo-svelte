8be9f6f5445b569d4641bf71c3ef509dce9d0611
Forthtilliath <forth@live.fr>
2024-02-15 18:38:49 +0100

    feat: rename newId by id

--- src/lib/stores/toast.ts
+++ src/lib/stores/toast.ts
@@ -3,9 +3,9 @@ import { writable } from 'svelte/store';
 export const currentToasts = writable<Array<Toast.Item>>([]);
 
 export const addToast = ({ msg, type }: Toast.Options): void => {
-	const newId = crypto.randomUUID();
-	currentToasts.update((old) => [...old, { msg, type, id: newId }]);
-	setTimeout(() => deleteToast(newId), 3000);
+	const id = crypto.randomUUID();
+	currentToasts.update((old) => [...old, { msg, type, id }]);
+	setTimeout(() => deleteToast(id), 3000);
 };
 
 export const deleteToast = (id: string): void => {
