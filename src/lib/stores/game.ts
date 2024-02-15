a1581728d4ff870545b252a220c610415bc0c85f
Forthtilliath <forth@live.fr>
2024-02-15 18:36:56 +0100

    feat: create a store for game & remove currentFocus

--- src/lib/stores/game.ts
+++ src/lib/stores/game.ts
@@ -1,3 +1,71 @@
-import { writable } from 'svelte/store';
+import { writable, get } from 'svelte/store';
 
-export const currentFocus = writable<Game.Card | null>(null);
+// export const currentFocus = writable<Game.Card | null>(null);
+
+type PlayerOne = 0;
+type PlayerTwo = 1;
+type GameStore = {
+	gameId: DB.Game['game_id'] | null;
+	players: Array<DB.User['id']>;
+	playerTurn: PlayerOne | PlayerTwo;
+	// board: Array<Game.Position>;
+	focus: Game.Card | null;
+	drag: Game.PlayerCard | null;
+};
+
+const gameStore = () => {
+	const store = writable<GameStore>({
+		gameId: null,
+		players: [],
+		playerTurn: 0,
+		// board: [],
+		focus: null,
+		drag: null
+	});
+
+	return {
+		...store,
+
+		get drag(): Game.PlayerCard | null {
+			return get(this).drag;
+		},
+
+		setGameId: (id: string) => {
+			store.update((store) => {
+				return {
+					...store,
+					gameId: id
+				};
+			});
+		},
+
+		setFocus: (card: Game.Card | null) => {
+			store.update((store) => {
+				return {
+					...store,
+					focus: card
+				};
+			});
+		},
+
+		setDrag: (card: Game.PlayerCard | null) => {
+			store.update((store) => {
+				return {
+					...store,
+					drag: card
+				};
+			});
+		},
+
+		toggleTurn: () => {
+			store.update((store) => {
+				return {
+					...store,
+					playerTurn: store.playerTurn === 0 ? 1 : 0
+				};
+			});
+		}
+	};
+};
+
+export const game = gameStore();
