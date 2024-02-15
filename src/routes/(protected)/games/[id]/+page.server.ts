d10d40a5b92c6acaaec4902a33c120eb51eab5be
Forthtilliath <forth@live.fr>
2024-02-15 18:39:28 +0100

    fix: remove fail which make some errors (need to be upgrade)

--- src/routes/(protected)/games/[id]/+page.server.ts
+++ src/routes/(protected)/games/[id]/+page.server.ts
@@ -1,16 +1,31 @@
 import db from '$lib/data/db';
 import { generateBoard } from '$lib/game.js';
-import { fail } from '@sveltejs/kit';
+
+type ErrorMessage = {
+	status: number;
+	message: string;
+};
+
+type Data = {
+	status: number;
+	board: Game.Square[];
+};
 
 export const load = async ({ params, parent }) => {
 	await parent();
 	const rawData = await db.games.get(params.id);
-	if (!rawData) return fail(404);
+	if (!rawData) {
+		return {
+			status: 404,
+			message: 'Game not found'
+		} satisfies ErrorMessage;
+	}
 
 	const board: Game.Square[] = generateBoard(rawData);
 	// TODO obfuscation selon le user
 
 	return {
+		status: 200,
 		board
-	};
+	} satisfies Data;
 };
