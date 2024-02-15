6293cae9e8d4290115373df679a51114844022ee
Coding-Jarl <senvisage@gmail.com>
2024-02-15 21:24:32 +0100

    fix: implement a Singleton for socket client

--- src/lib/methods/subscribeSocket.ts
+++ src/lib/methods/subscribeSocket.ts
@@ -3,27 +3,32 @@ import type { Socket } from 'socket.io-client';
 import { io } from 'socket.io-client';
 import { writable } from 'svelte/store';
 
+let socket: Socket<ServerToClientEvents, ClientToServerEvents> | null = null;
+
 export function subscribeSocket(room = 'waiting') {
 	const messages = writable<Array<DB.Message>>([]);
-	const socket: Socket<ServerToClientEvents, ClientToServerEvents> = io();
 
-	socket.emit('joinRoom', room);
+	if (!socket) {
+		socket = io();
 
-	socket.on('serverNotice', (msg) => {
-		addToast({ msg, type: 'notice' });
-	});
-	socket.on('lastMessages', (msg) => {
-		messages.set(msg);
-	});
-	socket.on('newMessage', (msg) => {
-		messages.update((m) => [...m, msg]);
-	});
-	socket.on('newAction', (act) => {
-		addToast({
-			msg: `Succesfully dropped ${act.cardinstance_id} into ${act.destination}`,
-			type: 'notice'
+		socket.emit('joinRoom', room);
+
+		socket.on('serverNotice', (msg) => {
+			addToast({ msg, type: 'notice' });
 		});
-	});
+		socket.on('lastMessages', (msg) => {
+			messages.set(msg);
+		});
+		socket.on('newMessage', (msg) => {
+			messages.update((m) => [...m, msg]);
+		});
+		socket.on('newAction', (act) => {
+			addToast({
+				msg: `Succesfully dropped ${act.cardinstance_id} into ${act.destination}`,
+				type: 'notice'
+			});
+		});
+	}
 
 	return { socket, messages };
 }
