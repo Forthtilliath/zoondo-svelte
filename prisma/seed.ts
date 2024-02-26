b5466c9e6b79b314bb8c916a992b13564465b734
Coding-Jarl <senvisage@gmail.com>
2024-02-26 22:44:27 +0100

    chore: cleanup old DB migrations

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,8 +1,36 @@
 import { PrismaClient } from '@prisma/client';
+import crypto from 'crypto';
 const prisma = new PrismaClient();
 
 async function main() {
 	// TODO Fill this ?
+	await prisma.user.upsert({
+		where: { email: 'evoli@spamland.com' },
+		update: {},
+		create: {
+			id: crypto.randomUUID(), //SQLite
+			email: 'evoli@spamland.com',
+			username: 'Evoli'
+		}
+	});
+	await prisma.user.upsert({
+		where: { email: 'aquali@spamland.com' },
+		update: {},
+		create: {
+			id: crypto.randomUUID(), //SQLite
+			email: 'aquali@spamland.com',
+			username: 'Aquali'
+		}
+	});
+	await prisma.user.upsert({
+		where: { email: 'pyroli@spamland.com' },
+		update: {},
+		create: {
+			id: crypto.randomUUID(), //SQLite
+			email: 'pyroli@spamland.com',
+			username: 'Pyroli'
+		}
+	});
 }
 
 main()
