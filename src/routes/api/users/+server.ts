85866c3949b90953aa151c1ab49bca284b309fc0
Coding-Jarl <senvisage@gmail.com>
2024-02-29 18:44:21 +0100

    refactor: remove useless /api routes

--- src/routes/api/users/+server.ts
+++
@@ -1,9 +0,0 @@
-import { json } from '@sveltejs/kit';
-import { PrismaClient } from '@prisma/client';
-const prisma = new PrismaClient();
-
-export async function GET() {
-	const users = await prisma.user.findMany();
-
-	return json(users);
-}
