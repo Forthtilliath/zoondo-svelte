d084f68d64eb5d0fd1381bc130b0fd73d98641fe
Forthtilliath <forth@live.fr>
2024-03-11 15:56:16 +0100

    style: change input color to black

--- src/routes/(public)/(auth)/signin/+page.svelte
+++ src/routes/(public)/(auth)/signin/+page.svelte
@@ -5,9 +5,9 @@
 <h1>Sign in</h1>
 <form method="post" use:enhance>
   <label for="username">Username</label>
-  <input name="username" id="username" /><br />
+  <input name="username" class="text-slate-950" id="username" /><br />
   <label for="password">Password</label>
-  <input type="password" name="password" id="password" /><br />
+  <input type="password" class="text-slate-950" name="password" id="password" /><br />
   <input type="submit" />
 </form>
 <a href="/signup">Create an account</a>
--- src/routes/(public)/(auth)/signup/+page.svelte
+++ src/routes/(public)/(auth)/signup/+page.svelte
@@ -5,11 +5,11 @@
 <h1>Sign up</h1>
 <form method="post" use:enhance>
   <label for="username">Username</label>
-  <input name="username" id="username" /><br />
+  <input name="username" class="text-slate-950" id="username" /><br />
   <label for="username">Email</label>
-  <input name="email" id="email" /><br />
+  <input name="email" class="text-slate-950" id="email" /><br />
   <label for="password">Password</label>
-  <input type="password" name="password" id="password" /><br />
+  <input type="password" class="text-slate-950" name="password" id="password" /><br />
   <input type="submit" />
 </form>
 <a href="/signin">Sign in</a>
