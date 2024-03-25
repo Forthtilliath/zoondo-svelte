1d99781ff43a0256ce5e685c1428b438112cc12d
Coding-Jarl <senvisage@gmail.com>
2024-03-25 23:16:56 +0100

    Refactor: Name WSocket actions in prevision to later file splitting

--- src/lib/server/websocket.ts
+++ src/lib/server/websocket.ts
@@ -4,12 +4,81 @@ import db from '../data/db';
 import { generateBoard } from '../game';
 import { availableCards } from '../data/mock';
 
+let io: Server<ClientToServerEvents, ServerToClientEvents>;
+
+const broadcastMsg: (room:string)=>ClientToServerEvents['message'] = (room)=> async (content, author_id) => {
+  const msg: DB.MessageCreate = {
+    id: crypto.randomUUID(),
+    author_id,
+    room,
+    content
+  };
+  const newMsg = await db.messages.create(msg);
+  io.to(room).emit('newMessage', newMsg);
+}
+
+// TODO: rename function !
+const playAction = async (action: DB.Action) => {
+  //TODO Validation de l'action
+  const attacker = await db.cardInstances.get(action.cardinstance_id);
+  if (!attacker) return;
+  const attackerCard = availableCards[attacker.card_id]
+ 
+  console.log(`${attackerCard.name} is moving to ${action.destination}`);
+
+  const [x, y] = action.destination.split(";").map(txt=>Number(txt))
+  const defenderCard = board.find( (sq)=> sq.x===x && sq.y===y)?.card
+  if(defenderCard)
+    console.log(`${defenderCard.name} is ready to receive him!`);
+  else{
+    console.log(`No problem, since this square is ${defenderCard}`);
+  }
+
+  //WIP Trigger combat + resolution
+  if(defenderCard) {
+    const attackerValue = attackerCard.corners[Math.floor(Math.random()*4)]
+    const defenderValue = defenderCard.corners[Math.floor(Math.random()*4)]
+
+    if(attackerValue==="*") {
+      const resolver = await db.cards.getResolver(attackerCard.slug);
+      resolver()
+      console.log("attacker resolved *");              
+    }
+    else if(defenderValue==="*") {
+      const resolver = await db.cards.getResolver(defenderCard.slug);
+      resolver()
+      console.log("defender resolved *");              
+    }
+    else {
+      if(attackerValue>defenderValue) console.log("The attacker wins !");
+      else console.log("The defender wins !");
+    }
+  }
+
+  const act: DB.ActionCreate = { ...action, action_id: crypto.randomUUID() };
+  const newAct = await db.actions.create(act).catch((err) => {
+    console.error(err);
+  });
+  if (!newAct) return;
+
+  const gameData = await db.games.getExtended(action.game_id);
+  if (gameData) {
+    const board = generateBoard(gameData);
+
+    io.to(room).emit('syncAction', {
+      board,
+      nextActionRestrictions: null
+    });
+  }
+}
+
 export default {
   name: 'webSocketServer',
   configureServer(server: ViteDevServer) {
     if (!server.httpServer) return;
 
-    const io = new Server<ClientToServerEvents, ServerToClientEvents>(server.httpServer);
+    if(!io)
+      io = new Server<ClientToServerEvents, ServerToClientEvents>(server.httpServer);
 
     io.on('connection', (socket) => {
       socket.emit('serverNotice', 'Hello World !');
@@ -24,70 +93,9 @@ export default {
 
         socket.emit('lastMessages', await db.messages.getByRoom(room));
 
-        socket.on('message', async (content, author_id) => {
-          const msg: DB.MessageCreate = {
-            id: crypto.randomUUID(),
-            author_id,
-            room,
-            content
-          };
-          const newMsg = await db.messages.create(msg);
-          io.to(room).emit('newMessage', newMsg);
-        });
+        socket.on('message', broadcastMsg(room));
 
-        socket.on('pushAction', async (action) => {
-          //TODO Validation de l'action
-          const attacker = await db.cardInstances.get(action.cardinstance_id);
-          if (!attacker) return;
-          const attackerCard = availableCards[attacker.card_id]
-         
-          console.log(`${attackerCard.name} is moving to ${action.destination}`);
-
-          const [x, y] = action.destination.split(";").map(txt=>Number(txt))
-          const defenderCard = board.find( (sq)=> sq.x===x && sq.y===y)?.card
-          if(defenderCard)
-            console.log(`${defenderCard.name} is ready to receive him!`);
-          else{
-            console.log(`No problem, since this square is ${defenderCard}`);
-          }
-
-          //TODO Trigger combat + resolution
-          if(defenderCard) {
-            const attackerValue = attackerCard.corners[Math.floor(Math.random()*4)]
-            const defenderValue = defenderCard.corners[Math.floor(Math.random()*4)]
-
-            if(attackerValue==="*") {
-              const resolver = await db.cards.getResolver(attackerCard.slug);
-              resolver()
-              console.log("attacker resolved *");              
-            }
-            else if(defenderValue==="*") {
-              const resolver = await db.cards.getResolver(defenderCard.slug);
-              resolver()
-              console.log("defender resolved *");              
-            }
-            else {
-              if(attackerValue>defenderValue) console.log("The attacker wins !");
-              else console.log("The defender wins !");
-            }
-          }
-
-          const act: DB.ActionCreate = { ...action, action_id: crypto.randomUUID() };
-          const newAct = await db.actions.create(act).catch((err) => {
-            console.error(err);
-          });
-          if (!newAct) return;
-
-          const gameData = await db.games.getExtended(action.game_id);
-          if (gameData) {
-            const board = generateBoard(gameData);
-
-            io.to(room).emit('syncAction', {
-              board,
-              nextActionRestrictions: null
-            });
-          }
-        });
+        socket.on('pushAction', playAction);
       });
     });
   }
