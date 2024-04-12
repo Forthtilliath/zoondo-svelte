537dd61cfb295b87d76f7989fbe331a9397e5d32
Forthtilliath <forth@live.fr>
2024-04-12 18:12:38 +0200

    ✨ feat: add more check about parameters

--- src/lib/methods/isType.ts
+++ src/lib/methods/isType.ts
@@ -1,3 +1,6 @@
+export function isRoomGame(room: Room): room is RoomGame;
 export function isRoomGame(room: Room): room is RoomGame {
-    return room.startsWith('game#');
-  }
+  if( room === undefined ) throw new Error('isRoomGame() requires 1 parameter');
+  
+  return /^game#[0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12}$/i.test(room);
+}
